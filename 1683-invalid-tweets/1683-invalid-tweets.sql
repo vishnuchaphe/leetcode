e66a5c79a0351ea2c07af2346c6f6018d41edef7
Vishnu <vishnuchaphe0@gmail.com>
2026-07-19 08:03:55 +0530

    Time: 575 ms (87.95%), Space: 0B (100%) - LeetHub - Time: O(N), Space: O(N)

---
+++ 1683-invalid-tweets/1683-invalid-tweets.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select tweet_id from Tweets where length(content)>15;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
